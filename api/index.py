0863ebc9a9dd643b0f64ddcdecd6143de7b6c66e
wxydejoy <1798923421@qq.com>
2022-01-03 16:45:14 +0800

    5e

--- api/index.py
+++ api/index.py
@@ -79,7 +79,8 @@ class handler(BaseHTTPRequestHandler):
         self.send_header('Access-Control-Allow-Origin', '*')
         self.send_header('Content-type', 'application/json')
         self.end_headers()
-        self.wfile.write(json.dumps(data).encode('utf-8'))
+        self.wfile.write(data)
         return
+
 
 
